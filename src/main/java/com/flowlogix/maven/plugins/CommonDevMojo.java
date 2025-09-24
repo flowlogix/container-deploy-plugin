596fcbf34aea15b0631757da24133cd1ad5a2355
lprimak <lenny@flowlogix.com>
2025-09-24 18:51:36 -0500

    feat: add Javadoc comments for clarity in CommonDevMojo, Deployer, DeployMojo, DevMojo, UndeployMojo, and Watcher classes

--- src/main/java/com/flowlogix/maven/plugins/CommonDevMojo.java
+++ src/main/java/com/flowlogix/maven/plugins/CommonDevMojo.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.flowlogix.maven.plugins;
 
 import org.apache.maven.execution.MavenSession;
@@ -15,6 +33,9 @@ import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
 import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
 
+/**
+ * Common parameters and code for dev and redeploy mojos
+ */
 abstract class CommonDevMojo extends AbstractMojo {
     @Inject
     MavenProject project;
@@ -25,21 +46,39 @@ abstract class CommonDevMojo extends AbstractMojo {
     @Inject
     BuildPluginManager pluginManager;
 
+    /**
+     * Administration URL to connect to the Server
+     */
     @Parameter(defaultValue = "http://localhost:4848", property = "payara.adminUrl")
     String payaraAminURL;
 
+    /**
+     * HTTP port where the application is deployed
+     */
     @Parameter(defaultValue = "8080", property = "payara.httpPort")
     String payaraHttpPort;
 
+    /**
+     * Force deployment even if the server says the application is already deployed
+     */
     @Parameter(defaultValue = "false", property = "payara.force")
     boolean force;
 
+    /**
+     * Whether to include libraries from server's lib/warlibs directory
+     */
     @Parameter(defaultValue = "true", property = "payara.warlibs")
     boolean warlibs;
 
+    /**
+     * Whether to enable availability on the deployed application
+     */
     @Parameter(defaultValue = "true", property = "payara.availabilityenabled")
     boolean availabilityenabled;
 
+    /**
+     * Whether to keep the state of the application on redeploy (secondary option to availabilityenabled)
+     */
     @Parameter(defaultValue = "false", property = "payara.keepstate")
     boolean keepstate;
 
--- src/main/java/com/flowlogix/maven/plugins/DevMojo.java
+++ src/main/java/com/flowlogix/maven/plugins/DevMojo.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.flowlogix.maven.plugins;
 
 import com.flowlogix.maven.plugins.Deployer.CommandResult;
@@ -11,6 +29,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
+/**
+ * Goal which deploys application in dev mode, opens browser and monitors
+ * src/main for changes. On change, recompiles and redeploys code if needed,
+ * Works for both Payara and GlassFish servers.
+ */
 @Mojo(name = "dev", requiresProject = false, threadSafe = true,
         requiresDependencyResolution = ResolutionScope.COMPILE,
         requiresDependencyCollection = ResolutionScope.COMPILE)
